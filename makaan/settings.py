f968f00b715a78dfc51ac63ad46e770c41cfd833
= <=>
2023-07-14 14:25:10 +0530

    changed the static root variable

--- makaan/settings.py
+++ makaan/settings.py
@@ -121,7 +121,7 @@ import os
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(
     BASE_DIR,
     'static'
