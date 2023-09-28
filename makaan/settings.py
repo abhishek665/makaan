0d8949953e77d158ed5a9390b88413cff5922fbd
= <=>
2023-09-28 18:53:56 +0530

    added postgres db

--- makaan/settings.py
+++ makaan/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'makaan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'makaan',            # Replace with your database name
+        'USER': 'abhi',   # Replace with your PostgreSQL username
+        'PASSWORD': 'abhi', # Replace with your PostgreSQL password
+        'HOST': 'localhost',       # Replace with your database host (usually 'localhost' for local development)
+        'PORT': '',                # Leave empty to use the default PostgreSQL port (5432)
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
