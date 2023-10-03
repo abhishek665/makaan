e62ec4d4e518618e08752ba24dcbb94aad6122f8
= <=>
2023-10-03 21:46:37 +0530

    returned self.first_name instead of username

--- ghar/models.py
+++ ghar/models.py
@@ -13,7 +13,7 @@ class Agent(models.Model):
     address = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.username.username
+        return self.first_name
 
 
 class Property(models.Model):
