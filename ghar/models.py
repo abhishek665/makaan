70eb04899d8d2d859a6a312b7984e4f15c058b10
= <=>
2023-10-03 21:42:08 +0530

    linked agents with User model

--- ghar/models.py
+++ ghar/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Agent(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
     address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username.username
 
 
 class Property(models.Model):
